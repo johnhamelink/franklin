c9cc72f76d83ec5625b1cdf64867df8b9c11eeef
serv-inc <serv-inc@users.noreply.github.com>
2017-03-31 01:35:52 +0200

    crashed on empty field, lacked logo

--- app/src/main/java/onion/logplusbmixd5zjl/EditLog.java
+++ app/src/main/java/onion/logplusbmixd5zjl/EditLog.java
@@ -23,54 +23,58 @@ public class EditLog extends Activity {
     private LogEntry logEntry;
 
     @Override public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
-	Bundle extras = getIntent().getExtras();
-	if (extras == null) {
-	    Common.showToast(this, "called without log entry to edit");
-	    finish();
-	}
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            Common.showToast(this, "called without log entry to edit");
+            finish();
+        }
 
-	Common.init(this);
+        Common.init(this);
 
-	int position = extras.getInt("position");
-	logEntry = LogEntry.getReversed(this, position);
+        int position = extras.getInt("position");
+        logEntry = LogEntry.getReversed(this, position);
 
-	setContentView(R.layout.edit_log); 
+        setContentView(R.layout.edit_log);
 
-	commentView  = (EditText) findViewById(R.id.editlog_comment);
-	durationView = (EditText) findViewById(R.id.editlog_duration);
-	nameView     = (EditText) findViewById(R.id.editlog_name);
+        commentView  = (EditText) findViewById(R.id.editlog_comment);
+        durationView = (EditText) findViewById(R.id.editlog_duration);
+        nameView     = (EditText) findViewById(R.id.editlog_name);
 
-	commentView.setText(logEntry.getComment());
-	commentView.requestFocus();
-	durationView.setText(logEntry.getDuration() / 1000 + "");
-	durationView.addTextChangedListener(new TextValidator(durationView) {
-		@Override public void validate(TextView textView, String text) {
-		    TextValidator.validatePositiveNumber(EditLog.this,
-							 textView, text);
-		}
-	    });
-	nameView.setText(logEntry.getName());
+        commentView.setText(logEntry.getComment());
+        commentView.requestFocus();
+        durationView.setText(logEntry.getDuration() / 1000 + "");
+        durationView.addTextChangedListener(new TextValidator(durationView) {
+                @Override public void validate(TextView textView, String text) {
+                    TextValidator.validatePositiveNumber(EditLog.this,
+                                                         textView, text);
+                }
+            });
+        nameView.setText(logEntry.getName());
     }
 
     @Override public void onPause() {
-	log.debug("onPause()");
-	long durationTmp = -1;
-	try {
-	    durationTmp = Long.parseLong(durationView.getText().toString());
-	} catch ( NumberFormatException e ) {
-	    throw new RuntimeException(e + "should be caught by inputtype-xml");
-	}
-	
-	boolean changed = logEntry.save(nameView.getText().toString(),
-					durationTmp * 1000,
-					commentView.getText().toString());
+        log.debug("onPause()");
+        long durationTmp = -1;
+        try {
+            durationTmp = Long.parseLong(durationView.getText().toString());
+        } catch ( NumberFormatException e ) {
+            Common.showToast(this,
+                             String.format("%s not readable as duration: %s",
+                                           durationView.getText(), e));
+            super.onPause();
+            return;
+        }
 
-	if ( changed ) {
-	    Common.showToast(this, getResources().getString(R.string.saved));
-	    setResult(Activity.RESULT_OK, new Intent());
-	}
-	super.onPause();
+        boolean changed = logEntry.save(nameView.getText().toString(),
+                                        durationTmp * 1000,
+                                        commentView.getText().toString());
+
+        if ( changed ) {
+            Common.showToast(this, getResources().getString(R.string.saved));
+            setResult(Activity.RESULT_OK, new Intent());
+        }
+        super.onPause();
     }
 }
