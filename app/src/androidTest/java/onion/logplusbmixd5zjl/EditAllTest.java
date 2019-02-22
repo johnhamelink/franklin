e290bd0089856df96d5c266f17e90b5cd9db12aa
serv-inc <serv-inc@users.noreply.github.com>
2019-02-22 13:54:40 +0100

    lacked editall, which lacked some formatting

---
+++ app/src/androidTest/java/onion/logplusbmixd5zjl/EditAllTest.java
@@ -0,0 +1,21 @@
+package onion.logplusbmixd5zjl;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import onion.logplusbmixd5zjl.data.MetaTest;
+
+@RunWith(AndroidJUnit4.class)
+public class EditAllTest extends MetaTest {
+    @Test
+    public void test_creation() throws Exception {
+        //EditAll e = new CountEntry(getContext(), "testc_creation", 12);
+        //assertEquals("testc_creation", e.getName());
+        //assertEquals(12, e.getTarget());
+    }
+}
