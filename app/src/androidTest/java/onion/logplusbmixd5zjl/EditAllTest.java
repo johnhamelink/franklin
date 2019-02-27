6f89aef6afce903d0f77946ad0f70b49eb5d2a6b
serv-inc <serv-inc@users.noreply.github.com>
2019-02-27 17:01:17 +0100

    test: lacked first editalltest (plus first release after a long time)

--- app/src/androidTest/java/onion/logplusbmixd5zjl/EditAllTest.java
+++ app/src/androidTest/java/onion/logplusbmixd5zjl/EditAllTest.java
@@ -1,7 +1,9 @@
 package onion.logplusbmixd5zjl;
 
+import android.content.Intent;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,8 +14,17 @@ import onion.logplusbmixd5zjl.data.MetaTest;
 
 @RunWith(AndroidJUnit4.class)
 public class EditAllTest extends MetaTest {
+    @Rule
+    public final ActivityTestRule<EditAll> rule =
+            new ActivityTestRule<>(EditAll.class, true, false);
+
     @Test
     public void test_creation() throws Exception {
+        Intent i = new Intent(getInstrumentation().getTargetContext(), EditAll.class)
+                .putExtra(EditAll.NAMES, new String[]{"name", "time"})
+                .putExtra(EditAll.TYPES, new String[]{"string", "date"});
+        getInstrumentation().getTargetContext().startActivity(i);
+
         //EditAll e = new CountEntry(getContext(), "testc_creation", 12);
         //assertEquals("testc_creation", e.getName());
         //assertEquals(12, e.getTarget());
